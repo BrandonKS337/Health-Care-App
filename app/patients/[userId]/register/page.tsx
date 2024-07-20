2b3802354a31801311c1b353e1335a6b3248e306
Project V2 <brandonks337@gmail.com>
2024-07-20 13:16:16 -0500

    Registration page started. Going to push forward with entering in all the different form fields before next push.

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,11 +1,11 @@
+import { RegisterForm } from "@/components/forms/RegisterForm";
+import { getUser } from "@/lib/actions/patient.actions";
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 
-import { PatientForm } from "@/components/forms/PatientForm";
-// import { PasskeyModal } from "@/components/PasskeyModal";
-
-const Home = ({ searchParams }: SearchParamProps) => {
-  const isAdmin = searchParams?.admin === "true";
+const Registration = async ({ params: { userId } }: SearchParamProps) => {
+  const user = await getUser("userId");
 
   return (
     <div className="flex h-screen max-h-screen">
@@ -22,7 +22,8 @@ const Home = ({ searchParams }: SearchParamProps) => {
             className="mb-12 h-10 w-fit"
           />
 
-          <PatientForm />
+          {/* <PatientForm /> */}
+          <RegisterForm />
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
@@ -36,14 +37,14 @@ const Home = ({ searchParams }: SearchParamProps) => {
       </section>
 
       <Image
-        src="/assets/images/onboarding-img.png"
+        src="/assets/images/register-img.png"
         height={1000}
         width={1000}
         alt="patient"
-        className="side-img max-w-[50%]"
+        className="side-img max-w-[390px]"
       />
     </div>
   );
 };
 
-export default Home;
+export default Registration;
