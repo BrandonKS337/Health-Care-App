d676856a1e07aace1b00816819b8ce3b0840a077
Project V2 <brandonks337@gmail.com>
2024-07-21 23:45:23 -0500

    added in admin login modal. this uses encryption and decryption to store an accesskey in localStorage so users can just click admin and get taken to the portal right away after first successfull entry.

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { PatientForm } from "@/components/forms/PatientForm";
-// import { PasskeyModal } from "@/components/PasskeyModal";
+import PasskeyModal from "@/components/ui/PasskeyModal";
 
 const Home = ({ searchParams }: SearchParamProps) => {
   const isAdmin = searchParams?.admin === "true";
 
   return (
     <div className="flex h-screen max-h-screen">
-      {/* {isAdmin && <PasskeyModal />} */}
-      {/* TODO: OTP Verification | PasskeyModal */}
+      {isAdmin && <PasskeyModal />}
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
