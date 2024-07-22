d90ac5c73a01d19a1321b01214f0dc3f6836b66a
Project V2 <brandonks337@gmail.com>
2024-07-22 01:02:01 -0500

    Id patient status appointment and doctor columns down. Need to move on to the dropdown table and then push forward from there. TS 4:08:43. Issue with doctors?.images was due to import error from wrong package. keep this in mind moving forward.

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,15 @@
 import StatCard from "@/components/StatCard";
+import {columns, Payment} from "@/components/table/columns";
+import {DataTable} from "@/components/table/DataTable";
 import { getRecentAppointmentList } from "@/lib/actions/appointement.actions";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+
+
 const admin = async () => {
-const appointments = await getRecentAppointmentList()
+  const appointments = await getRecentAppointmentList();
 
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
@@ -32,25 +36,27 @@ const appointments = await getRecentAppointmentList()
         </section>
 
         <section className="admin-stat">
-        <StatCard
-        type='appointments'
-        count={appointments.scheduledCount}
-        label='Scheduled appointments'
-        icon='/assets/icons/appointments.svg'
-        />
-        <StatCard
-        type='pending'
-        count={appointments.pendingCount}
-        label='Pending appointments'
-        icon='/assets/icons/pending.svg'
-        />
-        <StatCard
-        type='cancelled'
-        count={appointments.cancelledCount}
-        label='Cancelled appointments'
-        icon='/assets/icons/cancelled.svg'
-        />
+          <StatCard
+            type="appointments"
+            count={appointments.scheduledCount}
+            label="Scheduled appointments"
+            icon="/assets/icons/appointments.svg"
+          />
+          <StatCard
+            type="pending"
+            count={appointments.pendingCount}
+            label="Pending appointments"
+            icon="/assets/icons/pending.svg"
+          />
+          <StatCard
+            type="cancelled"
+            count={appointments.cancelledCount}
+            label="Cancelled appointments"
+            icon="/assets/icons/cancelled.svg"
+          />
         </section>
+
+        <DataTable columns={columns} data={appointments.documents}/>
       </main>
     </div>
   );
---
+++ components/table/columns.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "../ui/button";
+import StatusBadge from "../StatusBadge";
+import { formatDateTime } from "@/lib/utils";
+import { Doctors } from "@/constants";
+import Image from "next/image";
+import { MoreHorizontal } from "lucide-react";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    header: "ID",
+    cell: ({ row }) => <p className="text-14-medium">{row.index + 1}</p>,
+  },
+  {
+    accessorKey: "patient",
+    header: "Patient",
+    cell: ({ row }) => {
+      return <p className="text-14-medium">{row.original.patient.name}</p>;
+    },
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => (
+      <div className="min-w-[115px]">
+        <StatusBadge status={row.original.status} />
+      </div>
+    ),
+  },
+  {
+    accessorKey: "schedule",
+    header: "Appointment",
+    cell: ({ row }) => (
+      <p className="text-14-regular min-w-[100px]">
+        {formatDateTime(row.original.schedule).dateTime}
+      </p>
+    ),
+  },
+  {
+    accessorKey: "primaryPhysician",
+    header: "Doctor",
+    cell: ({ row }) => {
+      const appointment = row.original;
+
+      const doctor = Doctors.find(
+        (doctor) => doctor.name === appointment.primaryPhysician
+      );
+
+      return (
+        <div className="flex items-center gap-3">
+          <Image
+            src={doctor?.image!}
+            alt="doctor"
+            width={100}
+            height={100}
+            className="size-8"
+          />
+          <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+        </div>
+      );
+    },
+  },
+  
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
