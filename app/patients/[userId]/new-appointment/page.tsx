4afc8e17c5006a507f4d91476861adcc89c08490
Project V2 <brandonks337@gmail.com>
2024-07-21 19:55:23 -0500

    got through new-appointment page and able to route users through to the success page! Will push on to render this success page next.

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
 
-const NewAppointment = () => {
+import { PatientForm } from "@/components/forms/PatientForm";
+import { AppointmentForm } from "@/components/forms/AppointmentForm";
+import { getPatient } from "@/lib/actions/patient.actions";
+// import { PasskeyModal } from "@/components/PasskeyModal";
+
+export default async function NewAppointment({params: {userId}}: SearchParamProps) {
+    const patient = await getPatient(userId)
+
   return (
-    <div>NewAppointment</div>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          {/* <PatientForm /> */}
+          {/* Appointment Form */}
+          <AppointmentForm
+          type='create'
+          userId={userId}
+          patientId={patient.$id}
+
+          />
+
+            <p className="copyright mt-10 py-12">
+              © 2024 CarePluse
+            </p>
+          
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
   )
 }
 
-export default NewAppointment
