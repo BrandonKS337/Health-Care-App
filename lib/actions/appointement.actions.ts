89c70a9ea7dbe734defc3dec119eb5e91c17c03e
Project V2 <brandonks337@gmail.com>
2024-07-22 00:14:38 -0500

    admin counter added. shows scheduled, pending, cancelled currently entered. Pushing on to the dashboard grouping next.

--- lib/actions/appointement.actions.ts
+++ lib/actions/appointement.actions.ts
@@ -1,12 +1,13 @@
-'use server'
+"use server";
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
   databases,
 } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
 
 export const createAppointment = async (
   appointment: CreateAppointmentParams
@@ -25,16 +26,56 @@ export const createAppointment = async (
   }
 };
 
-
 export const getAppointment = async (appointmentId: string) => {
-    try {
-        const appointment = await databases.getDocument(
-            DATABASE_ID!,
-            APPOINTMENT_COLLECTION_ID!,
-            appointmentId,
-        )
-        return parseStringify(appointment)
-    } catch (error) {
-        console.log(error)
+  try {
+    const appointment = await databases.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+    return parseStringify(appointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getRecentAppointmentList = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc("$createdAt")]
+    );
+
+    const initialCounts = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    };
+
+    const counts = (appointments.documents as Appointment[]).reduce(
+      (acc, appointment) => {
+        if (appointment.status === "scheduled") {
+          acc.scheduledCount += 1;
+        } else if (appointment.status === "pending") {
+          acc.pendingCount += 1;
+        } else if (appointment.status === "cancelled") {
+          acc.cancelledCount += 1;
+        }
+
+        return acc;
+      },
+      initialCounts
+    );
+
+    const data = { 
+      totalCount: appointments.total,
+      ...counts,
+      documents: appointments.documents
     }
-}
+
+    return parseStringify(data)
+  } catch (error) {
+    console.log(error);
+  }
+};
